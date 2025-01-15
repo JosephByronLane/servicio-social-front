5c4c2e67c50bdac9cb069814512358e7cf792646
CookieMaster2 <danielsamvalla@gmail.com>
2025-01-15 02:46:43 -0600

    Primera iteración de las páginas terminada

---
+++ app/scripts/house-info/house-info-scripts.js
@@ -0,0 +1,6 @@
+function openModal(imageSrc) {
+    document.getElementById('modalImage').src = imageSrc;
+    var myModal = new bootstrap.Modal(document.getElementById('imageModal'));
+    myModal.show();
+  }
+  
